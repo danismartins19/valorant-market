cc03ef421b66486b4ed25c1a9d6595d6bb1c8280
Daniel Martins <danidiamoo1@gmail.com>
2022-06-23 19:37:23 -0300

    smg component stylized

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import styles from "./App.module.css";
 import background from "./assets/background.png";
 import { Pistol } from "./components/Pistol";
+import weapons from './lib/weapons.json';
 import pistols from './lib/pistols.json';
-import weapons from './lib/weapons.json'
+import smgs from './lib/smgs.json';
+import shotguns from './lib/shotguns.json';
+import rifles from './lib/rifles.json';
+import snipers from './lib/snipers.json';
+import machines from './lib/machines.json';
 import { useState, useEffect } from "react";
+import { Smg } from "./components/Smg";
 
 const App = () => {
 
@@ -39,6 +45,9 @@ const App = () => {
 
             <div className={styles.smgColumn}>
               <p className={styles.banner}>SMGs</p>
+              {smgs.map((smg)=>(
+                <Smg ID={smg.ID} name={smg.Name} url={smg.weapon_asset.url} price={smg.Stats.CredCost} key={smg.ID} onHover={hoverWeapon} />
+              ))}
             </div>
 
             <div className={styles.shotgunColumn}>
