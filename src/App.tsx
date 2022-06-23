66f1671a3524e5aa382f6bd6551e4589cc014233
Daniel Martins <danidiamoo1@gmail.com>
2022-06-22 22:15:56 -0300

    structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
+import styles from "./App.module.css";
+import background from "./assets/background.png";
+
 const App = () => {
-  
+
 
   return (
-    <>
-      
-    </>
+    <div className={styles.body}>
+      <img src={background} alt="Imagem de fundo" className={styles.imgBack} />
+
+      <div className={styles.market}>
+        <div className={styles.marketArea}>
+
+
+          <div className={styles.pistolColumn}>
+            <p className={styles.banner}>PISTOLA</p>
+            
+          </div>
+
+          <div className={styles.column}>
+
+
+            <div className={styles.smgColumn}>
+              <p className={styles.banner}>SMGs</p>
+            </div>
+
+            <div className={styles.shotgunColumn}>
+              <p className={styles.banner}>SHOTGUNS</p>
+            </div>
+
+
+          </div>
+
+          <div className={styles.rifleColumn}>
+            <p className={styles.banner}>RIFLES</p>
+          </div>
+
+          <div className={styles.column}>
+
+
+            <div className={styles.sniperColumn}>
+              <p className={styles.banner}>SNIPERS</p>
+            </div>
+
+            <div className={styles.machineColumn}>
+              <p className={styles.banner}>MACHINE GUNS</p>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/Weapon/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+
+type Props = {
+    name: string,
+    price: number,
+    url: string
+}
+
+export const Weapon = ({name, price, url}: Props) =>{
+    return(
+        <div className={}></div>
+    )
+}
